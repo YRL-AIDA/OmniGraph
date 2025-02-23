56af4c0b63ba1355f04d5d3f20c858286a6c2bd6
poddubnyyiv <poddubnyyiv@yandex.ru>
2025-02-24 00:08:20 +0800

    Доработан скрипт формирования датасета для задачи тапекса на графе. К алгоритму прикручено семплирование датасета для обработки и распараллеливание обработки

--- TapexGraph/Convert_TAPEX_data_to_GRAPH_v2_parallel.py
+++ TapexGraph/Convert_TAPEX_data_to_GRAPH_v2_parallel.py
@@ -4,72 +4,83 @@
 # In[9]:
 
 
-from datasets import load_dataset,load_from_disk,Dataset
+from datasets import concatenate_datasets,load_from_disk,Dataset
+
 from add_utils import translate_query_to_graph_form
 #from datasets.utils.logging import disable_progress_bar
 #disable_progress_bar()
 from concurrent.futures import ProcessPoolExecutor
+import itertools
 from tqdm import tqdm
 # Чтение данных из файло
-with open('tapex_pretrain/train.src', 'r', encoding='utf-8') as src_file:
-    questions = src_file.read().splitlines()
-with open('tapex_pretrain/train.tgt', 'r', encoding='utf-8') as tgt_file:
-    answers = tgt_file.read().splitlines()
+def read_questions(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            yield line.strip()
+#questions = list(read_questions('tapex_pretrain/train.src'))
+#answers = list(read_questions('tapex_pretrain/train.tgt'))
 # Проверка, что количество вопросов и ответов совпадает
-assert len(questions) == len(answers), "Количество вопросов и ответов должно совпадать."
+#assert len(questions) == len(answers), "Количество вопросов и ответов должно совпадать."
 # Создание словаря для Dataset
-data = {
-    'question': questions,
-    'answer': answers
-}
+
 # Создание Dataset
-dataset = Dataset.from_dict(data)
-
-print("Загрузил данные")
-# In[10]:
-
-#i = 0
-#j = 0
 omega_include = ["P","C","S","GB","H","OB","A","OP","L"]#
-
-
-# In[27]:
 
 
 def map_function_for_question_change(example):
     try:
         #print(example['question'])
-        processed_question,answer = translate_query_to_graph_form(example['question'],
-                                                                  answer = example['answer'],
-                                                                  Omega_include=omega_include)
-
+        example['question'],example['answer'] = translate_query_to_graph_form(example['question'],
+                                                                              answer = example['answer'],
+                                                                              Omega_include=omega_include)
     except Exception as e:
-
-        processed_question = "None"
-        answer = "None"
+            example['question'] = "None"
+            example['answer'] = "None"
     finally:
-        return {'question': processed_question, 'answer': answer}
+        return example
+start_chank_id = 8
+chank_id = 0
+bach_size = 200000
+questions_gen = read_questions('tapex_pretrain/train.src')
+answ_gen = read_questions('tapex_pretrain/train.tgt')
+for i in range(start_chank_id):
+    list(itertools.islice(questions_gen, bach_size))
+    list(itertools.islice(answ_gen, bach_size))
+chank_id = start_chank_id
+bach_size = 100000
+list(itertools.islice(questions_gen, bach_size))
+list(itertools.islice(answ_gen, bach_size))
+while True:
+    question = list(itertools.islice(questions_gen, bach_size))
+    answer = list(itertools.islice(answ_gen, bach_size))
+    print(f'----------------{chank_id}---------------------')
+    if not question:
+        break
+    dataset = Dataset.from_dict({
+        'question': question,
+        'answer': answer
+    })
+    print("Загрузил данные")
+    print("Start processing")
+    dataset = dataset.map(map_function_for_question_change,num_proc=10)
+    print(dataset[0])
 
-def process_dataset_in_parallel(dataset, max_workers=4):
-
-    with ProcessPoolExecutor(max_workers=max_workers) as executor:
-
-        # Применяем функцию обработки к каждому элементу в датасете
-
-        processed_data = list(executor.map(map_function_for_question_change, dataset))
-
-    
-
-    # Возвращаем обработанный датасет
-
-    return Dataset.from_list(processed_data)
-# In[ ]:
-
-
-
-#dataset = dataset.map(map_function_for_question_change)
-print("Start processing")
-processed_dataset = process_dataset_in_parallel(dataset,max_workers=16)
+    print("DATA TRANSFORMED. START SAVING")
+    dataset.save_to_disk(f'./converved_to_{"".join(omega_include).lower()}_graph_tapex_data_{chank_id}')
+    chank_id+=1
+# In[10]:
+dataset = Dataset.from_dict({
+        'question': [],
+        'answer': []
+    })
+for chank in range(chank_id):
+    dataset_path = f'./converved_to_{"".join(omega_include).lower()}_graph_tapex_data_{chank}'
+    data = load_from_disk(dataset_path)
+    dataset = concatenate_datasets([dataset,data])
+    del data
 print("DATA TRANSFORMED. START SAVING")
-dataset.save_to_disk(f'./converved_to_{"".join(omega_include).lower()}_graph_tapex_data')
+dataset.save_to_disk(f'./converved_to_{"".join(omega_include).lower()}_graph_tapex_data_full')
+
+
+
 
